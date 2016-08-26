ce31217e0923686ce8eb0c9281406b027a70d007
johnlonganecker <johnlonganecker@gmail.com>
2016-08-25 18:41:26 -0700

    remove v from version number

--- mockaroon.rb
+++ mockaroon.rb
@@ -3,7 +3,7 @@ require "FileUtils"
 class Mockaroon < Formula
   homepage "https://github.com/geofffranks/spruce"
 
-  v = "v0.0.1"
+  v = "0.0.1"
   url "https://github.com/johnlonganecker/mockaroon/releases/download/#{v}/mockaroon-darwin-amd64"
   version v
   sha256 "91e90e790808d5f444fa42795891d65b569885fca056572d925c3f4b9db1cc11"
